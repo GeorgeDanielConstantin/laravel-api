c0662204758485ec196dbaca159bc9037f90d5cf
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-04-18 19:49:00 +0200

    working layout index + show

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.app')
+
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 
 /*
@@ -18,6 +19,13 @@ use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 
 Route::get('/dashboard', [AdminHomeController::class, 'index'])->middleware(['auth'])->name('dashboard');
 Route::get('/', [GuestHomeController::class, 'index']);
+
+Route::middleware('auth')
+->prefix('/admin')
+->name('admin.')
+->group(function() {
+    Route::resource('projects', ProjectController::class);
+});
 
 Route::middleware('auth')
 ->prefix('profile')
